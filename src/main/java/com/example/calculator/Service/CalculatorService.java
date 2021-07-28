607c6190d90e443f006169e64f8997813fb762ae
VietHLe <87550192+VietHLe@users.noreply.github.com>
2021-07-28 15:44:14 -0400

    second commit

--- src/main/java/com/example/calculator/Service/CalculatorService.java
+++ src/main/java/com/example/calculator/Service/CalculatorService.java
@@ -5,10 +5,46 @@ import com.example.calculator.Repository.CalculatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CalculatorService {
     @Autowired
     private CalculatorRepository calcRepository;
 
-    public
+    public List<CalculatorModel> getAll(){
+        return calcRepository.findAll();
+    }
+    public CalculatorModel add(Long id, CalculatorModel plus){
+        CalculatorModel total = calcRepository.findById(id).orElse(null);
+        float oldTotal = total.getWant();
+        float add = plus.getAdd();
+        float newTotal = oldTotal + add;
+        total.setWant(newTotal);
+        return calcRepository.save(total);
+    }
+    public CalculatorModel sub(Long id, CalculatorModel subtract){
+        CalculatorModel total = calcRepository.findById(id).orElse(null);
+        float oldTotal = total.getWant();
+        float sub = subtract.getSubtract();
+        float newTotal = oldTotal - sub;
+        total.setWant(newTotal);
+        return calcRepository.save(total);
+    }
+    public CalculatorModel div(Long id, CalculatorModel divide){
+        CalculatorModel total = calcRepository.findById(id).orElse(null);
+        float oldTotal = total.getDivide();
+        float newDiv = divide.getDivide();
+        float newTotal = oldTotal / newDiv;
+        total.setWant(newTotal);
+        return calcRepository.save(total);
+    }
+    public CalculatorModel multi(Long id, CalculatorModel multiply){
+        CalculatorModel total = calcRepository.findById(id).orElse(null);
+        float oldTotal = total.getWant();
+        float newMultiply = multiply.getMultiply();
+        float newTotal = oldTotal * newMultiply;
+        total.setWant(newTotal);
+        return calcRepository.save(total);
+    }
 }
